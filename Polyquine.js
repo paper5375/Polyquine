19b996a50f873ffe513119aac88727de0df171a8
graph-paper <54546213+graph-paper@users.noreply.github.com>
2021-03-12 14:49:36 -0500

    Add polyquine

---
+++ Polyquine.js
@@ -0,0 +1,4 @@
+//\u000A\u002F\u002A
+function f() {//\u002A\u002Fpublic class Polyquine {public static void main(String[] args) {var s = "//%1$cu000a%1$cu002F%1$cu002A%4$sfunction f() {//%1$cu002A%1$cu002Fpublic class Polyquine {public static void main(String[] args) {var s = %2$c%3$s%2$c;System.out.printf(s, 92, 34, s, %2$c%1$cn%2$c);}}%4$s// %1$cu000A%1$cu002F%1$cu002A%4$sconsole.log(`//%1$c%1$cu000A%1$c%1$cu002F%1$c%1$cu002A%1$cn${f};f()//%1$c%1$cu002A%1$c%1$cu002F`);}f()//%1$cu002A%1$cu002F";System.out.printf(s, 92, 34, s, "\n");}}
+// \u000A\u002F\u002A
+console.log(`//\\u000A\\u002F\\u002A\n${f};f()//\\u002A\\u002F`);} f()//\u002A\u002F
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Polyquine
- Prints its own source code in JavaScript and Java
+  This is a program that prints its own source code when run in JavaScript and Java. This is by no means the smallest or best of its kind, it exists as a proof of concept.
