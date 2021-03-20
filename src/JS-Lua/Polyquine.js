42189683e7419809287fee8c1f690ce29eba5541
graph-paper <54546213+graph-paper@users.noreply.github.com>
2021-03-20 11:22:41 -0400

    Organize

--- src/JS-Lua/Polyquine.js
+++ src/JS-Lua/Polyquine.js
@@ -5,4 +5,4 @@ var a=_=>{
 /*--]]=1
 i="\n"
 s="n=3%sn--/*%s--[[*///%svar a=_=>{%s/*--]]=1%si=\"\\n\"%ss=%qprint(s:format(s))%s--[[%s*/console.log(`n=3\\nn--/*\\n\\-\\-\\[\\[*/\\nvar a=${a};a()//\\-\\-\\]\\]`);};a()//--]]"print(s:format(i,i,i,i,i,i,s,i,i))--[[
-console.log(`n=3\nn--/*\n\-\-\[\[*/\nvar a=${a};a()//\-\-\]\]`);};a()//--]]
+console.log(`n=3\nn--/*//\n\-\-\[\[*/\nvar a=${a};a()//\-\-\]\]`);};a()//--]]
